e99e08baf2aa06bf388f1b17ca4152e43df32914
camerona6581 <78610579+camerona6581@users.noreply.github.com>
2024-02-15 14:36:28 -0500

    Add files via upload

---
+++ P1LAB1_CameronAnthony.py
@@ -0,0 +1,13 @@
+# Anthony Cameron
+# 2/15/2024
+# P1LAB1
+# This program will display my name in a welcome message
+
+# get the first name from the user and assign it to a variable
+fname = input("Enter your first name: ")
+# get the first name from the user and assign it to a variable
+lname = input("Enter your second name: ")
+# print out the names in a  welcome message
+print("Hello,",fname,lname + '! Welcome to CTI-110')
+print("Hello," + ' ' + fname + ' ' + lname + '! Welcome to CTI-110')
+input("Press any key to exit")
